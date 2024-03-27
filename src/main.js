4600bd2fa974d4d06a5a1b6d806ec5afc7b78a50
Van Nguyen <VanNgnRan@gmail.com>
2024-03-27 09:33:08 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// import "./assets/main.css";
+// import "./assets/form.css";
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import router from "./router";
+createApp(App).use(router).mount("#app");
+// createApp(App).mount("#app");
